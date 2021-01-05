32e8c354c5a5d087b4f3e0337adef9e4d6fb4580
MaryamBanytlebi <Banytalebi.m@gmail.com>
2021-01-05 05:35:40 -0800

    Create Navigation drawer

--- app/src/main/java/org/maktab/digikala/controller/activities/HomePageActivity.java
+++ app/src/main/java/org/maktab/digikala/controller/activities/HomePageActivity.java
@@ -1,14 +1,62 @@
 package org.maktab.digikala.controller.activities;
 
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import org.maktab.digikala.controller.fragments.HomePageFragment;
+import android.os.Bundle;
+import android.view.Menu;
 
-public class HomePageActivity extends SingleFragmentActivity {
+import com.google.android.material.navigation.NavigationView;
 
+import org.maktab.digikala.R;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
-    public Fragment createFragment() {
-        return HomePageFragment.newInstance();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+       /* FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
---
+++ app/src/main/java/org/maktab/digikala/controller/fragments/CategoryFragment.java
@@ -0,0 +1,85 @@
+package org.maktab.digikala.controller.fragments;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.maktab.digikala.R;
+import org.maktab.digikala.adapter.CategoryAdapter;
+import org.maktab.digikala.databinding.FragmentCategoryBinding;
+import org.maktab.digikala.model.ProductCategory;
+import org.maktab.digikala.repository.ProductRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryFragment extends Fragment {
+
+    private ProductRepository mRepository;
+    private List<ProductCategory> mCategoryList;
+    private CategoryAdapter mCategoryAdapter;
+    private LiveData<List<ProductCategory>> mCategoryItemsLiveData;
+    private FragmentCategoryBinding mBinding;
+
+    public CategoryFragment() {
+        // Required empty public constructor
+    }
+
+    public static CategoryFragment newInstance() {
+        CategoryFragment fragment = new CategoryFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mCategoryList = new ArrayList<>();
+        mRepository = new ProductRepository();
+        mRepository.fetchCategoryItemsAsync();
+        mCategoryItemsLiveData = mRepository.getProductCategoryLiveData();
+
+        setObserver();
+    }
+
+    private void setObserver() {
+        mCategoryItemsLiveData.observe(this, new Observer<List<ProductCategory>>() {
+            @Override
+            public void onChanged(List<ProductCategory> categories) {
+                mCategoryList.addAll(categories);
+                setAdapter(mCategoryList);
+            }
+        });
+    }
+
+    private void setAdapter(List<ProductCategory> categories) {
+        mCategoryAdapter = new CategoryAdapter(getActivity(), categories);
+        mBinding.recyclerCategory.setAdapter(mCategoryAdapter);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_category, container,
+                false);
+
+        initViews();
+        return mBinding.getRoot();
+    }
+
+    private void initViews() {
+        mBinding.recyclerCategory.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+}
